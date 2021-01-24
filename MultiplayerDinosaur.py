caf183f439458c75f88d466d038c98077607a24c
Bandi72006 <bandi72006@gmail.com>
2021-01-24 13:43:04 +0400

    Added jump mechanic that is WAY too high but can be fixed with maths

--- MultiplayerDinosaur.py
+++ MultiplayerDinosaur.py
@@ -1,5 +1,6 @@
 #todo list:
 #Implement isJumped condition (boolean value)
+#added comments so it's readable (duh)
 
 import pygame
 import random
@@ -13,22 +14,27 @@ xPos = 250
 yPos = 250
 xVel = 0
 yVel = 0
+isJump = False
+jumpCounter = 1
 
 #Sets up FPS manager to keep it at 60 always
 fpsClock = pygame.time.Clock()
 FPS = 30
 
+def Jump(yValue, jump):
+    if isJump == True:
+        if jump <= 5: #max jump height
+            yValue -= (1/jump*2)*50
+        elif jump >= 10 and jump <= 15:
+            yValue += (jump-10)*15
+        else:
+            jump = 1
 
-def Jump(yValue):
-    if (yValue > 200): #200 = height of jump
-        for i in range(50):
-            yValue = yValue - 1
-            print(yValue)
-            return yValue
+    else:
+        jump = 1
 
     return yValue
-
-
+        
 run = True
 
 while run:
@@ -37,14 +43,24 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    yPos = Jump(yPos, jumpCounter)
+    jumpCounter += 1
+    if jumpCounter > 15:
+        isJump = False
+    print(isJump)
+
+
     #Input handling
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_SPACE]:
-        yPos = Jump(yPos)
+        if isJump == False:
+            isJump = True
+            jumpCounter = 1
 
     #drawing stuff    
     screen.fill((255,255,255)) 
+    pygame.draw.rect(screen, (0,0,0), (250, yPos, 10, 10))
      
     pygame.display.update()
     
