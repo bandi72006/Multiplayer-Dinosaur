8bda9e410e4a126dbbb8eb15fbe372ecc33871a3
Bandi72006 <bandi72006@gmail.com>
2021-01-25 08:32:15 +0400

    Added basic cactus object. No collision yet but will be added soon

--- MultiplayerDinosaur.py
+++ MultiplayerDinosaur.py
@@ -5,7 +5,6 @@
 
 import pygame
 import random
-import numpy
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
@@ -21,6 +20,25 @@ jumpCounter = 1
 #Sets up FPS manager to keep it at 60 always
 fpsClock = pygame.time.Clock()
 FPS = 30
+
+class cactusObject(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(1,3)
+
+    def move(self):
+        if self.x < 0:
+            self.x = 1280
+        self.x -= 10
+
+    def draw(self):
+        pygame.draw.rect(screen, (100,255,100), (self.x, self.y, 20, 50))
+
+
+
+cactus = cactusObject(1280, 360)
+
 
 def Jump(yValue, jump):
     if isJump == True:
@@ -47,17 +65,21 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+
+    #Movement
     yPos = Jump(yPos, jumpCounter)
     jumpCounter += 1
     if jumpCounter > 25: #5 extra frames for a tiny delay
         isJump = False
+
+    cactus.move()
 
 
 
     #Input handling
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
         if isJump == False:
             isJump = True
             jumpCounter = 1
@@ -65,6 +87,7 @@ while run:
     #drawing stuff    
     screen.fill((255,255,255)) 
     pygame.draw.rect(screen, (0,0,0), (640, yPos, 10, 50))
+    cactus.draw()
 
     #screen.blit(defaultSprite, (640, yPos)) REALLY BIG! DON'T ADD YET!
 
